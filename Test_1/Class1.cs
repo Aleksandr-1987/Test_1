03e2e6d407f2fda6aa63567bfdebb19324ab5c1a
Aleksandr <aleksander13@ukr.net>
2021-10-15 21:00:56 +0300

    Добавьте файлы проекта.

---
+++ Test_1/Class1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_1
+{
+    class Class1
+    {
+        public static bool IsCorrect1(string str)
+        {
+            if (str.StartsWith("COUNT <<") || str.StartsWith("SIZE <<") || str.StartsWith("PATH <<"))
+                return true;
+            return false;
+        }
+
+        public static bool IsCorrect2(string str)
+        {
+            if (str.EndsWith(">>"))
+                return true;
+            return false;
+        }
+
+        public static char Select(string command)
+        {
+            char com;
+            if (command.StartsWith("COUNT <<")) com = '1';
+            else if (command.StartsWith("SIZE <<")) com = '2';
+            else com = '3';
+            return com;
+        }
+
+        public static void Count(string com, string dirName)
+        {
+            com = com.Remove(0, 8); com = com.Remove(com.Length - 2, 2);
+            
+            if(com == "")
+            {
+                string[] files = Directory.GetFiles(dirName);
+                int count = 0;
+                foreach (string s in files)
+                    count++;
+
+                Console.WriteLine();
+                Console.WriteLine("Количество отсканированных файлов: " + count);
+                Console.WriteLine();
+                Console.WriteLine("Чтобы продолжить вводить команды нажмите любую клавишу;");
+                Console.WriteLine("Чтобы выйти нажмите - 0;");
+            }
+            else
+            {
+                string[] files = Directory.GetFiles(dirName, "*." + com);
+                int count = 0;
+                foreach (string s in files)
+                    count++;
+
+                Console.WriteLine();
+                Console.WriteLine("Количество отсканированных файлов с заданным расширением: " + count);
+                Console.WriteLine();
+                Console.WriteLine("Чтобы продолжить вводить команды нажмите любую клавишу;");
+                Console.WriteLine("Чтобы выйти нажмите - 0;");
+            }            
+        }
+
+        public static void Size(string com, string dirName)
+        {
+            com = com.Remove(0, 7); com = com.Remove(com.Length - 2, 2);
+
+            if (com == "")
+            {
+                DirectoryInfo di = new DirectoryInfo(dirName);
+                FileInfo[] fiArr = di.GetFiles();
+                long maxsize = 0;
+                foreach (FileInfo f in fiArr)
+                    maxsize += f.Length;
+
+                Console.WriteLine();
+                Console.WriteLine("Общий размер файлов: " + maxsize + " байтов.");
+                Console.WriteLine();
+                Console.WriteLine("Чтобы продолжить вводить команды нажмите любую клавишу;");
+                Console.WriteLine("Чтобы выйти нажмите - 0;");
+            }
+            else
+            {
+                DirectoryInfo di = new DirectoryInfo(dirName);
+                FileInfo[] fiArr = di.GetFiles("*." + com);
+                long maxsize = 0;
+                foreach (FileInfo f in fiArr)
+                    maxsize += f.Length;
+
+                Console.WriteLine();
+                Console.WriteLine("Общий размер файлов с заданным расширением: " + maxsize + " байтов.");
+                Console.WriteLine();
+                Console.WriteLine("Чтобы продолжить вводить команды нажмите любую клавишу;");
+                Console.WriteLine("Чтобы выйти нажмите - 0;");
+            }            
+        }
+
+        public static void Path(string com, string dirName)
+        {
+            com = com.Remove(0, 7); com = com.Remove(com.Length - 2, 2);
+
+            if (com == "")
+            {
+                Console.WriteLine();
+                Console.WriteLine("Список всех файлов с полным путем:");
+                string[] files = Directory.GetFiles(dirName);
+                foreach (string s in files)
+                    Console.WriteLine(s);
+                Console.WriteLine();
+                Console.WriteLine("Чтобы продолжить вводить команды нажмите любую клавишу;");
+                Console.WriteLine("Чтобы выйти нажмите - 0;");
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Список всех файлов с полным путем:");
+                string[] files = Directory.GetFiles(dirName, "*." + com);
+                foreach (string s in files)
+                    Console.WriteLine(s);
+                Console.WriteLine();
+                Console.WriteLine("Чтобы продолжить вводить команды нажмите любую клавишу;");
+                Console.WriteLine("Чтобы выйти нажмите - 0;");
+            }                                
+        }
+    }
+}
---
+++ Test_1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Test_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char go = '1';
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("Укажите путь к папке с файлами:");
+                string dirName = Console.ReadLine();
+
+                if (Directory.Exists(dirName))
+                {
+                    char go2 = '1';
+                    do
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Введите команду:");
+                        string command = Console.ReadLine();
+
+                        if (Class1.IsCorrect1(command) == true && Class1.IsCorrect2(command) == true)
+                        {
+                            char com = Class1.Select(command);
+                            switch (com)
+                            {
+                                case '1':                                    
+                                    Task task1 = Task.Run(() => Class1.Count(command, dirName));
+                                    break;
+                                case '2':
+                                    Task task2 = Task.Run(() => Class1.Size(command, dirName));
+                                    break;
+                                case '3':
+                                    Task task3 = Task.Run(() => Class1.Path(command, dirName));
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        else { Console.WriteLine("Вы ввели некорректную команду!");
+                            Console.WriteLine();
+                            Console.WriteLine("Чтобы продолжить вводить команды нажмите любую клавишу;");
+                            Console.WriteLine("Чтобы выйти нажмите - 0;");
+                        }                        
+
+                        string temp = Console.ReadLine();
+                        if (temp.Length > 1) temp = temp.Remove(1, temp.Length - 1);
+                        if (temp.Length == 0) temp = "1";
+                        go2 = Convert.ToChar(temp);
+                    } while (go2 != '0');                    
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Вы указали не существующую директорию!");                    
+                }
+                Console.WriteLine();
+                Console.WriteLine("Чтобы указать новый путь к папке с файлами нажмите любую клавишу;");
+                Console.WriteLine("Чтобы выйти нажмите - 0;");
+
+                string temp2 = Console.ReadLine();
+                if (temp2.Length > 1) temp2 = temp2.Remove(1, temp2.Length - 1);
+                if (temp2.Length == 0) temp2 = "1";
+                go = Convert.ToChar(temp2);
+            } while (go != '0');            
+        }
+    }
+}
